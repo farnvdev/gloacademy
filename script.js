f91a935be0f35beb1aeeb818ae2e1da62e8f585d
Fareed Vanas <fareednv@rambler.ru>
2019-08-23 11:28:14 +0300

    Variable modified

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 
 alert("alert 2");
 console.log("Hello world 2");
-let myVariable = 10;
+let myVariable = {
+    money : '15$',
+    income : '--',
+    addExpenses : '--',
+    deposit : '--',
+    mission : '--',
+    period : '--' 
+};
