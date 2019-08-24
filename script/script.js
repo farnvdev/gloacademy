526825991a408801e7548c6289668c3a1278e164
Fareed Vanas <fareednv@rambler.ru>
2019-08-24 20:15:11 +0300

    lession02

--- script/script.js
+++ script/script.js
@@ -1,11 +1,9 @@
-
-alert("alert 2");
-console.log("Hello world 2");
-let myVariable = {
-    money : '15$',
-    income : '--',
-    addExpenses : '--',
-    deposit : '--',
-    mission : '--',
-    period : '--' 
-};
+let money = 30000, Income = "Translation", addExpences = "Food, Family, School";
+let deposit = false, mission = 200000, period = 12;
+console.log(typeof (money), typeof (Income), typeof (deposit));
+console.log("Income length:", Income.length);
+console.log(period + " ,", mission, " рублей");
+addExpences = addExpences.toLowerCase();
+console.log(addExpences.split(", "));
+let dayBudget = money / 30;
+console.log(dayBudget,' ', money % 30);
