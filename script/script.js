3c1e3cb54ff73d355060cd84811347936cd6c2ff
Fareed Vanas <fareednv@rambler.ru>
2019-09-01 22:46:18 +0300

    lesson4

--- script/script.js
+++ script/script.js
@@ -1,18 +1,34 @@
 'use strict';
+//pms
 let mission = prompt ('Сколько вы хотите заработать?', 10000),
-money = prompt ('Ваш месячный доход?', 10000),
-addExpences = prompt ('Перечислите возможные расходы за рассчитываемый период через запятую');
-console.log(addExpences.split(', '));
-let deposit = confirm('Есть ли у вас депозит в банке?');
-console.log(typeof(money),typeof(addExpences),typeof(deposit));
-let stctExp1 = prompt ('Какие обязательные ежемесячные расходы у вас есть?'),
+monthInc = prompt ('Ваш месячный доход?', 10000),
+addExpences = prompt ('Перечислите возможные расходы за рассчитываемый период через запятую'),
+deposit = confirm('Есть ли у вас депозит в банке?'),
+stctExp1 = prompt ('Какие обязательные ежемесячные расходы у вас есть?'),
 cost1 = prompt ('Во сколько это обойдется?'), 
 stctExp2 = prompt ('Какие обязательные ежемесячные расходы у вас есть?2'),
-cost2 = prompt ('Во сколько это обойдется?'),
-budgetMonth = money - cost1 - cost2,
-result =  Math.ceil(mission / budgetMonth);
-console.log('Mission will be reached in ' + result + ' month.');
-let dayBudget = budgetMonth / 30;
+cost2 = prompt ('Во сколько это обойдется?');
+//pms
+
+function getAccumulatedMonth () {
+    function getExpensesMonth () {
+        if (isNaN(cost1 && cost2)){
+            alert('Incorrect value');
+        } else {
+            return cost1 + cost2;
+        }
+    }
+    return monthInc - getExpensesMonth();
+}
+let accumulatedMonth = getAccumulatedMonth();
+
+function getTargetMonth() {
+return Math.ceil(mission / accumulatedMonth);
+}
+let targetMBudget = getTargetMonth();
+console.log('Mission will be reached in ' + targetMBudget + ' month.');
+console.log(typeof(money),typeof(addExpences),typeof(deposit));
+let dayBudget = accumulatedMonth / 30;
 console.log('Бюджет на день: '+ Math.floor(dayBudget));
 if (dayBudget <= 800){
     console.log('Высокий уровень дохода');
